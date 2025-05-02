49a48a4de752590a6fe73362343a9ee69a7e1915
lourduradjou <rajlourdu15@gmail.com>
2025-05-02 20:53:15 +0530

    Added resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -144,7 +144,7 @@ const Navbar = () => {
 					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
 						<a
 							className='flex justify-between items-center w-full text-gray-300'
-							href='/'
+							href='https://drive.google.com/file/d/1i5OD2z3H1PBNVp_hPZk2Hc6B1gpvR0eu/view?usp=sharing'
 						>
 							Resume <BsFillPersonLinesFill size={30} />
 						</a>
